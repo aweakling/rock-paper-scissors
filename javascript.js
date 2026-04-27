191e7761cbb648c236db324791c049da64c93a56
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-04-27 22:36:17 +0800

    Add ui in index and modify javascript

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
+const rpsButtons = document.querySelector("#rpsButtons");
+const buttons = document.querySelectorAll("button");
+// const rock = document.querySelector("#rock");
+// const paper = document.querySelector("#paper");
+// const scissors = document.querySelector("#scissors");
+
+const results = document.querySelector("#results");
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
+const battle = document.querySelector("#battle");
+const score = document.querySelector("#score");
+const winner = document.querySelector("#winner");
+
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -15,9 +29,9 @@ function getComputerChoice(){
   } 
 }
 
-function getHumanChoice(){
-  let choice = prompt("Rock, Paper, Scissors!!!").toLowerCase();
-
+function getHumanChoice(choice){
+  //let choice = prompt("Rock, Paper, Scissors!!!").toLowerCase();
+  
   if(choice === "rock"){
     return "rock";
   }
@@ -27,44 +41,55 @@ function getHumanChoice(){
   if(choice === "scissors"){
     return "scissors";
   }
-  
+
   return "Invalid choice";
 }
 
-function playRound(){
-  let humanChoice = getHumanChoice();
+function playRound(humanChoice){
   let computerChoice = getComputerChoice();
+
   console.log("Player: ",humanChoice);
+  player.textContent = "Player: " + humanChoice;
   console.log("Computer: ",computerChoice);
+  computer.textContent = "Computer: " + computerChoice;
 
   if(humanChoice === computerChoice){
     console.log("It's a tie!!!");
+    battle.textContent =  "It's a tie!!!";
   } else if(humanChoice === "rock"){
     if(computerChoice === "scissors"){
       humanScore++;
       console.log("Rock beats Scissors");
+      battle.textContent =  "Rock beats Scissors";
     }else{
       computerScore++;
       console.log("Paper beats rock");
+      battle.textContent = "Paper beats rock";
     }
   } else if(humanChoice === "paper"){
     if(computerChoice === "rock"){
       humanScore++;
       console.log("Paper beats rock");
+      battle.textContent = "Paper beats rock";
     }else{
       computerScore++;
       console.log("Scissors beats paper");
+      battle.textContent = "Scissors beats paper";
     }
   } else if(humanChoice === "scissors"){
     if(computerChoice === "paper"){
       humanScore++;
       console.log("Scissors beats paper");
+      battle.textContent = "Scissors beats paper";
     }else{
       computerScore++;
       console.log("Rock beats Scissors");
+      battle.textContent = "Rock beats Scissors";
     }
   }
-  return "Invalid Choice";
+  
+  console.log("Score: ", humanScore, "-", computerScore);
+  score.textContent = "Player: " + humanScore + "   Computer: " + computerScore;
 }
 
 function playGame(){
@@ -90,4 +115,19 @@ function playGame(){
     return "Player Score: " + humanScore + " Computer Score: " + computerScore;
 }
 
-console.log(playGame());
+buttons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    const humanChoice = getHumanChoice(e.target.id);
+    playRound(humanChoice);
+
+    if(humanScore >= 5){
+      console.log("Winner: PLAYER");
+      winner.textContent = "Winner: PLAYER"
+    } else if (computerScore >= 5) {
+      console.log("Winner: COMPUTER");
+      winner.textContent = "Winner: COMPUTER";
+    }
+  });
+});
+
+// console.log(playGame());
